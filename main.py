dc96a05134ab7d8dfda81f74b1d385231bc8d611
Amin <aminoffline@yahoo.com>
2021-08-13 00:33:56 +0430

    working

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 import datetime as dt
 year, month, day = input().split(sep="/")
 year, month, day = int(year), int(month), int(day)
-
+#print(year, month, day)
 #print(birthday.year , dt.date.today().year)
-def age(year,month,day):
+
+def Age(year,month,day):
     if month <= 12 and day <=31:
         birthday = dt.date(year, month, day)
         today = dt.date.today()
-        age = today.year - birthday.year
+        if today.month - birthday.month >=0 and today.day-birthday.day>=0 :
+            age = today.year - birthday.year
+        else:
+            age = today.year - birthday.year -1
         #age = int(age)
         return age
     else:
         return 'WRONG'
 
-
-print(age(year,month,day))
+print(Age(year,month,day))
